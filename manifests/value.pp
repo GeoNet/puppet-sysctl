cc6a86031f2abb2c1116df2818f930424b0ac240
Richard Guest <r.guest@gns.cri.nz>
2014-11-21 15:15:31 +1300

    Fix deprecated variable access.

--- manifests/value.pp
+++ manifests/value.pp
@@ -5,7 +5,7 @@ define sysctl::value (
 ) {
 
   $array = split($value,'[\s\t]')
-  $val1 = inline_template("<%= array.delete_if(&:empty?).flatten.join(\"\t\") %>")
+  $val1 = inline_template("<%= @array.delete_if(&:empty?).flatten.join(\"\t\") %>")
 
   $suppress_flag = $suppress ? {
     true    => '-e',
